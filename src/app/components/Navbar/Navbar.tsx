4a69a9dfa03bd638f1ed5101182fc5b6f7d774bb
ArjunRathod <ar885209@gmail.com>
2024-06-21 16:38:45 +0530

    initial

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className='navbar'>
+      
+    </nav>
+  );
+}
+
+export default Navbar;
